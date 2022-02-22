4a46f4478b8e51dc7669e16b64f1b0db60c1fbdf
Narayana-Brahmajyoti <nbrahmajyoti@gmail.com>
2022-02-21 22:47:52 -0300

    Aula 1 - Demonstração em sala com professor

---
+++ Front_End_2/Aula 1 - Vinculando HTML e JavaScript/index.js
@@ -0,0 +1,11 @@
+var nome = "Narayana Martins";
+
+console.log ("Olá, meu nome é"+ " " + nome);
+
+let nomes = ['Narayana',"Carlos",'Tatiane', 'João'];
+
+for(let i = 0; i<nomes.length; i++){
+    let nomeCapturado = nomes[i];
+    console.log(nomeCapturado)
+}
+console.log(nomes);
