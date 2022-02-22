793bde766f5878df08d54ff1ff41896b6418b43c
Narayana-Brahmajyoti <nbrahmajyoti@gmail.com>
2022-02-21 22:49:31 -0300

    Pratica Integradora - Somar enquanto percorre um array

---
+++ Front_End_2/Aula 1 - Vinculando HTML e JavaScript/mesaDeTrabalho.js
@@ -0,0 +1,18 @@
+// Mesa de trabalho Aula 1 Grupo 3 - 
+
+let array = [1, 2, 4, 8];
+let soma = array[0];
+
+for (let i = 1; i < array.length; i++) {
+    soma += array[i];
+    console.log(soma)
+}
+
+// let array = [1, 2, 4, 8];
+// let soma = 0;
+
+// for (let valor of array) {
+    // soma += valor;
+    // console.log(soma)
+// }
+
