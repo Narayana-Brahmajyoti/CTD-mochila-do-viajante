42a68d0fa7a5acda8fc05149066d7f9cab10672e
Narayana-Brahmajyoti <nbrahmajyoti@gmail.com>
2022-05-29 14:56:37 -0300

    arquivos java

---
+++ POO/Aula 2 - Variavéis/teste/out/production/src/Main.java
@@ -0,0 +1,36 @@
+public class Main {
+    public static void main(String[] args) {
+        int howOlden;
+        String animal, name, quantityFood, sound;
+
+        animal = "cachorro";
+        name = "Shesheu";
+        howOlden = 2;
+        quantityFood = "1kg e meio";
+        sound = "au au";
+
+        System.out.println(name + " " + howOlden + " anos.");
+        System.out.println(name + " come " + quantityFood + " e faz " + sound + ".");
+        System.out.println();
+
+        animal = "peixe";
+        name = "Nemo";
+        howOlden = 1;
+        quantityFood = "140 gramas";
+        sound = "glub glub";
+
+        System.out.println(name + " " + howOlden + " ano.");
+        System.out.println(name + " come " + quantityFood + " e faz " + sound + ".");
+        System.out.println();
+
+        animal = "gato";
+        name = "Snow";
+        howOlden = 3;
+        quantityFood = "meio quilo";
+        sound = "miau miau";
+
+        System.out.println(name + " " + howOlden + " anos.");
+        System.out.println(name + " come " + quantityFood + " e faz " + sound + ".");
+        System.out.println();
+    }
+}
