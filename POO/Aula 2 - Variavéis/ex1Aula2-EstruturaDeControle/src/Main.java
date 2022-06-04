bff04cdf396ff084bcf18ee9e52596b9f1468fd6
Narayana-Brahmajyoti <nbrahmajyoti@gmail.com>
2022-06-03 22:18:23 -0300

    commit teste

--- POO/Aula 2 - Variavéis/ex1Aula2-EstruturaDeControle/src/Main.java
+++ POO/Aula 2 - Variavéis/ex1Aula2-EstruturaDeControle/src/Main.java
@@ -1,12 +1,6 @@
 public class Main {
-    static boolean ehDivisivel( int numA, int numB){
-        boolean divisivel = true;
-
-        if (numA % numB == 0){
-            System.out.println("O número é divisível!");
-        } else {
-            System.out.println("O número não é divisível!");
-        }
-
+    static boolean eDivisivel( int n, int divisor){
+        return n % divisor == 0;
     }
+
 }
