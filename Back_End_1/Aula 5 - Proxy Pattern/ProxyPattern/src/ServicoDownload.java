25dd94e96c45205dd7745f361555e24529cd4d16
Narayana-Brahmajyoti <nbrahmajyoti@gmail.com>
2022-08-06 08:31:29 -0300

    deletando arquivos duplicados - aula 5

--- Back_End_1/Aula 5 - Proxy Pattern/ProxyPattern/src/ServicoDownload.java
+++
@@ -1,7 +0,0 @@
-public class ServicoDownload implements Download{
-
-    @Override
-    public void baixarMusica(Usuario usuario) {
-        System.out.println("O usuário " + usuario.getId() + " tem permissão para fazer o download.");
-    }
-}
