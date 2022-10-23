2c83fff0e173b3e8c284bc574f2127fdd48cd884
Narayana-Brahmajyoti <nbrahmajyoti@gmail.com>
2022-10-22 21:47:30 -0300

    Calculadora disparando eventos

---
+++ Front_End_2/Aula 9 - Disparando Eventos/script/multiplicar.js
@@ -0,0 +1,7 @@
+function multiplicar (numero1,numero2){
+    if(numero1 == 0 || numero2 == 0){
+        return("O resultado dessa operação é zero"); 
+    }else{
+        return(numero1 * numero2);
+    };
+    }
---
+++ Front_End_2/Aula 9 - Disparando Eventos/script/subtrair.js
@@ -0,0 +1,4 @@
+function subtrair (numero1,numero2){
+
+    return(numero1 - numero2);
+}
